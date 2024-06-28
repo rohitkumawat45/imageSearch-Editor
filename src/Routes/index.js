a527e960ac3e3d7ccc72a2a50e7424358d964575
Rohit Kumawat <rohitkumawat857@gmail.com>
2024-06-28 21:17:01 +0530

    changed from browserRouter to hashRouter

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,29 +1,18 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "../Components/Home";
 import Nextpage from "../Components/Nextpage";
 
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-caption" element={<Nextpage />} />
+      </Routes>
+    </HashRouter>
+  );
+}
 
-const router= createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>,
-      },
-      {
-        path: "/add-caption",
-        element: <Nextpage/>,
-      },
-])
-
-
-function Routes() {
-    return (
-      <>
-        <RouterProvider router={router} />
-      </>
-    );
-  }
-  
-  export default Routes;
-  
+export default App;
